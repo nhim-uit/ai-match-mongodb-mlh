422d8f628dbf4c895b5cb935107b1343a1f6d958
nhim-21730055 <165756103+nhim-uit@users.noreply.github.com>
2024-12-09 10:04:07 +0700

    added button to handle responses

--- home.py
+++ home.py
@@ -13,3 +13,17 @@ st.divider()
 
 # Add a subheader
 st.subheader('Fill the form to get matched with a techie')
+
+st.text_input('What is your name?')
+st.text_input('What are your programming language?')
+st.text_input('What is your favourite of framework?')
+st.text_input('What is your favourite tech conference or blog post?')
+st.text_input('Have you contributed to any open source, name projects?')
+st.text_input('What is your favourite tech company?')
+st.text_input('What is your favourite code editor?')
+
+if st.button('Submit'):
+    # generate potential matches for my tech form
+    st.success('Yay you have been matched with a techie!')
+else:
+    st.warning('Opps! Something went wrong!!!')
