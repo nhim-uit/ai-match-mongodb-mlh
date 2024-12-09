9577c24fbdd2191986ab244652976da188663198
nhim-21730055 <165756103+nhim-uit@users.noreply.github.com>
2024-12-09 09:46:23 +0700

    ui changes on home

---
+++ home.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+# Write the title of the app
+st.title('Matching app for techies')
+
+# Place a divider
+st.divider()
+
+# Write a text
+st.write('Welcome to my app, fill the form to get started!')
+
+st.divider()
+
+# Add a subheader
+st.subheader('Fill the form to get matched with a techie')
