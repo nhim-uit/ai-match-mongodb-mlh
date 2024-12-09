86372a1a6994dc94a50991209ffe06528cf29a1f
nhim-21730055 <165756103+nhim-uit@users.noreply.github.com>
2024-12-09 09:26:40 +0700

    Create mongo.py

---
+++ mongo.py
@@ -0,0 +1,21 @@
+
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MY_MONGODB_PASSWORD = os.getenv('MY_MONGODB_PASSWORD')
+uri = f"mongodb+srv://alexcs1610:{MY_MONGODB_PASSWORD}@cluster1.yhab7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1"
+
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+    
